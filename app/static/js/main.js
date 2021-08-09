fb7c511e85913de2592d40f367011af55be6a352
Adrian <adi.jobda@gmail.com>
2021-08-10 01:31:11 +0200

    Results RWD, code beautify

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.js-example-basic-multiple').select2();
 });
-function scrollDown(){
-    window.scrollBy(0,window.innerHeight);
+
+function scrollDown() {
+    window.scrollBy(0, window.innerHeight);
 }
 
-function submitForm(){
-    if($('.js-example-basic-multiple')[0].value.length > 0){
+function submitForm() {
+    if ($('.js-example-basic-multiple')[0].value.length > 0) {
         $('#searchForm').submit();
     }
 }
-
 window.addEventListener("keyup", detectButton);
 
-function detectButton(e){
-    if(e.code == "Enter"){
+function detectButton(e) {
+    if (e.code == "Enter") {
         submitForm()
     }
 }
--- app/static/js/results.js
+++ app/static/js/results.js
@@ -1,24 +1,21 @@
     topics = document.getElementsByClassName('topicsContent');
-    for (i = 0; i<topics.length; i++){
-        if(topics[i].getBoundingClientRect().height>120){
+    for (i = 0; i < topics.length; i++) {
+        if (topics[i].getBoundingClientRect().height > 120) {
             topics[i].style.height = topics[i].getBoundingClientRect().height - 120 + 'px'
         }
     }
 
-
-    function checkIfFooterFixed(){
+    function checkIfFooterFixed() {
         footerBottom = document.getElementsByTagName('footer')[0].getBoundingClientRect().bottom;
-        if(footerBottom<window.innerHeight){
+        if (footerBottom < window.innerHeight) {
             document.getElementsByTagName('footer')[0].style.position = 'fixed';
             document.getElementsByTagName('footer')[0].style.bottom = '0';
-        }else{
+        } else {
             document.getElementsByTagName('footer')[0].style.position = 'relative';
         }
     }
 
-    function returnToIndex(){
+    function returnToIndex() {
         window.location.href = "/";
-
     }
-
     checkIfFooterFixed();
